fc2aa305f06c6aa3562a5fa8b4cf49f5a9a4d58e
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2022-01-14 13:45:11 -0600

    commit tree 3f884984946ead28871fa0176d324023101df42e

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,53 @@
-# your code here
+require 'pry'
+
+class MySet
+    attr_accessor :hash
+    def initialize(enumerable = [])
+        @hash = {}
+        enumerable.each do |v|
+            hash[v] = true
+        end
+    end
+
+    def self.[](*values)
+        self.new values
+    end
+
+    def include?(value)
+        hash.has_key? value
+    end
+
+    def add(value)
+        hash[value] = true
+        self
+    end
+
+    def delete(value)
+        hash.delete value
+        self
+    end
+
+    def size
+        hash.size
+    end
+
+    def clear
+        self.hash = {}
+        self
+    end
+
+    def each
+        for key in hash do
+            yield(key[0])
+        end
+        self
+    end
+
+    def length
+        self.size
+    end
+
+    def inspect
+        p "#<#{self.class}: {#{hash.keys.join(', ')}}>"
+    end
+end
